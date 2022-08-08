6eaaf68af5932fe2efe342b5631f4bb491215872
rkessal <kessal.rayhan25@gmail.com>
2022-08-08 14:47:53 +0200

    logo fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React from "react"
+import logo from "../images/troll-face.png"
 
 
 const Header = () => {
     return (
         <div className="header">
-            <img src="./images/troll-face.png" className="header--logo"/>
+            <img src={logo} className="header--logo"/>
             <h3 className="header--title">Meme Generator</h3>
             <h4 className="header--subtitle">React Course - Project 3</h4>
         </div>
